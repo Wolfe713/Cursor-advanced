e4ba2da28a4d4df7ddbb63b1e8110066a5c914e2
Rehesha <79754737+Wolfe713@users.noreply.github.com>
2022-08-27 18:27:57 +0300

    Hw2

---
+++ HW2/hw2.js
@@ -0,0 +1,26 @@
+const N = +prompt('Вкажіть число');
+
+console.log(typeof N);
+console.log(Number.isInteger(N));
+const M = +prompt('Вкажіть число');
+
+console.log(typeof M);
+console.log(Number.isInteger(M));
+let evenNum = window.confirm('Пропускати парні числа?');
+let result = 0;
+if (evenNum) {
+    for (let i = N; i <= M; i++) {
+      if (i % 2 == 1) {
+         result += i;
+        } }
+}
+      else {
+    for (let i = N; i <= M; i++){
+        result += i;
+    }
+  } 
+   console.log (result) ;  
+  
+
+
+
